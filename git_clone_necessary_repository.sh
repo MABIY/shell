6707a468ee8bcf93e7294bf977a492c93c136c08
lh <jyfc7879@gmail.com>
2016-11-24 17:02:07 +0800

    git clone necessary repository

---
+++ git_clone_necessary_repository.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "clone necessary Repository"
+git clone  https://github.com/MABIY/read_java_practice_code.git ~/project/read_java_practice_code
+git clone  https://github.com/MABIY/Documents.git  ~/Documents
+git clone  https://github.com/MABIY/shell.git ~/shell
