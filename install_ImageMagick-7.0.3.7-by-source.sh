46d68e317a0d82686f9cd74304c5f6d7d3ba60cf
lh <jyfc7879@gmail.com>
2016-12-02 17:34:29 +0800

    change ImageMagick-7.0.3-7.tar.gz  to ImageMagick-7.0.3-8.tar.gz

--- install_ImageMagick-7.0.3.7-by-source.sh
+++ install_ImageMagick-7.0.3.7-by-source.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# install ImageMagick-7.0.3.7 on centos
+# install ImageMagick-7.0.3.8 on centos
 echo "start download ImageMagick-7.0.37"
 if curl -O https://fossies.org/linux/misc/ImageMagick-7.0.3-8.tar.gz
 then
- tar -zxvf ImageMagick-7.0.3-7.tar.gz
+ tar -zxvf ImageMagick-7.0.3-8.tar.gz
  # png jpg 等依赖包
  yum install -y tcl-devel.x86_64 ghostscript-devel.x86_64 bzip2-devel.x86_64 freetype-devel.x86_64 libtiff-devel.x86_64
  yum install -y libjpeg-devel
  yum install -y glib2-devel.x86_64 fontconfig-devel.x86_64 zlib-devel.x86_64 libwmf-devel.x86_64
  yum install -y gcc
- cd ImageMagick-7.0.3-7
+ cd ImageMagick-7.0.3-8
  if ./configure --prefix=/usr --with-bzlib=yes --with-fontconfig=yes --with-freetype=yes --with-gslib=yes --with-gvc=yes --with-jpeg=yes --with-jp2=yes --with-png=yes --with-tiff=yes
   then
   make clean
