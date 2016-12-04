d22fd24da896a3f6e11cc9bec64a595c56954649
MABIY <jyfc7879@gmail.com>
2016-12-04 20:51:31 +0800

    imageImage install extend package

--- install_ImageMagick-7.0.3.7-by-source.sh
+++ install_ImageMagick-7.0.3.7-by-source.sh
@@ -24,9 +24,22 @@ fi
 echo "download ImageMagick-7.0.3.7 error"
 
 
- # if error when make about jpeg
- # curl -o http://www.ijg.org/files/jpegsrc.v9.tar.gz
- #cd jpeg-9
- #./configure
- #make libdir=/usr/lib64
- #make libdir=/usr/lib64 install
+#无网络环境通过包安装支持格式jng jpeg png
+#cd jpeg-9
+#./configure
+#make libdir=/usr/lib64
+#make libdir=/usr/lib64 install
+#
+#
+#cd zlib-1.2.8
+#./configure
+#make && make install
+#
+#cd libpng-1.6.26 
+#cp scripts/makefile.std makefile
+#make libdir=/usr/lib64
+#make libdir=/usr/lib64 install
+#
+
+
+
