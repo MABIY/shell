dae4ee170afa1437f64e7eb263f71143187358fc
lh <jyfc7879@gamil.com>
2018-05-11 20:57:23 +0800

    ..

---
+++ bbr/README.sh
@@ -0,0 +1,4 @@
+wget --no-check-certificate -qO 'BBR.sh' 'https://moeclub.org/attachment/LinuxShell/BBR.sh' && chmod a+x BBR.sh && bash BBR.sh -f
+wget --no-check-certificate -qO 'BBR_POWERED.sh' 'https://moeclub.org/attachment/LinuxShell/BBR_POWERED.sh' && chmod a+x BBR_POWERED.sh && bash BBR_POWERED.sh
+shadowsocks.service install
+
