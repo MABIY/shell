fa6244f50ca1c9a8c3c72b6f7b3ec39604e28492
lh <jyfc7879@gmail.com>
2018-06-15 15:35:38 +0800

    Update ubuntu_18.04_install.sh

--- ubuntu_18.04_install.sh
+++ ubuntu_18.04_install.sh
@@ -127,3 +127,5 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+#安装终端 terminator
+sudo apt-get install terminator
