f125dfba82ed7d48446e457373ee35477ea0c294
lh <jyfc7879@gmail.com>
2018-07-21 20:33:06 +0800

    Update ubuntu_18.04_install.sh

--- ubuntu_18.04_install.sh
+++ ubuntu_18.04_install.sh
@@ -111,7 +111,7 @@ sudo apt-get install chrome-gnome-shell -y
 echo "install grive2"
 sudo apt-get install git cmake build-essential libgcrypt11-dev libyajl-dev \
     libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev \
-    debhelper zlib1g-dev dpkg-dev pkg-config
+    debhelper zlib1g-dev dpkg-dev pkg-config -y
 git -C ~/ clone https://github.com/vitalif/grive2.git
 cd grive2 
 mkdir build
