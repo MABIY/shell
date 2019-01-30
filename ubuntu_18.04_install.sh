bc46e5c692c6d75abfe0bdd571ef172d549f129d
liuhua <liuhua@vipmro.net>
2019-01-30 16:12:56 +0800

    ..

--- ubuntu_18.04_install.sh
+++ ubuntu_18.04_install.sh
@@ -119,11 +119,6 @@ sudo make install
 rm -rf ~/grive2
 echo "success install grive2"
 
-echo "sublime text3"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install sublime-text -y
 #安装终端 terminator
 sudo apt-get install tmux -y
 #docker pull ilanyu/golang-reverseproxy
