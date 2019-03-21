2920a02402f4fc590c3d847478ae12fd569bf95f
liuhua <liuhua@vipmro.net>
2019-03-21 11:01:12 +0800

    ..

--- bbr/README.sh
+++
@@ -1,4 +0,0 @@
-wget --no-check-certificate -qO 'BBR.sh' 'https://moeclub.org/attachment/LinuxShell/BBR.sh' && chmod a+x BBR.sh && bash BBR.sh -f
-wget --no-check-certificate -qO 'BBR_POWERED.sh' 'https://moeclub.org/attachment/LinuxShell/BBR_POWERED.sh' && chmod a+x BBR_POWERED.sh && bash BBR_POWERED.sh
-shadowsocks.service install
-
