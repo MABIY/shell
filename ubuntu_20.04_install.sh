f492e2f8c4fb3f752aff0f1c725c8d9cd77996b7
liuhua <jyfc7879@gmail.com>
2020-04-27 13:39:10 +0800

    ..

---
+++ ubuntu_20.04_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "set lh not need input password"
+echo 'lh ALL=NOPASSWD: ALL' | sudo tee --append /etc/sudoers
+
+echo "更新系统 start"
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dis-upgrade
+sudo apt-get -y autoremove
+echo "更新系统 end"
+
+echo "install 安装文泉驿微黑字体库"
+sudo apt install -y ttf-wqy-microhei 
+echo "install git"
+sudo apt-get install -y git 
+git config --global user.email "jyfc7879@gmail.com"
+git config --global user.name "lh"
+echo "更新系统 end"
+
+echo "install curl"
+sudo apt-get install -y curl
+echo "installed curl"
+
+echo "install chrome-gnome-shell"
+sudo apt-get install -y chrome-gnome-shell 
+sudo apt install -y gnome-shell-extension-dashtodock
+echo "installed chrome-gnome-shell"
+
+echo "install tree"
+sudo apt install -y tree
+echo "installed tree"
+
+echo "install googlepinyin"
+sudo apt install fcitx-bin -y
+sudo apt install fcitx-googlepinyin -y
+echo "installed googlepinyin"
