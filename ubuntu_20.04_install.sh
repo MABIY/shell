605a6c7b4cf2cdcb2bc3339291a919a964a480ca
liuhua <jyfc7879@gmail.com>
2020-05-25 11:50:30 +0800

    ..

--- ubuntu_20.04_install.sh
+++ ubuntu_20.04_install.sh
@@ -166,6 +166,8 @@ sudo sh -c  'echo "gnome-session" >> /etc/xrdp/startwm.sh'
 gsettings set org.gnome.desktop.wm.keybindings panel-main-menu "[]" # disable Alt+F1
 gsettings set org.gnome.desktop.background picture-uri '' # remove default background image
 gsettings set org.gnome.desktop.background primary-color 'rgb(0, 102, 204)' # set blue background
+gsettings set org.gnome.desktop.screensaver picture-uri '' # remove default lock screen
+gsettings set org.gnome.desktop.screensaver primary-color 'rgb(0, 102, 204)' # set blue lock screen
 
 # install git tools ctrl+shift+p
 # sudo add-apt-repository ppa:peek-developers/stable -y
